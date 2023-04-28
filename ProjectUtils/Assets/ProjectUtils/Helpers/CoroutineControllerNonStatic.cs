3426ad4dc9afae0dbbbf56404e0fcb996b21700f
JoseGomis299 <joseg03a@gmail.com>
2023-04-28 17:59:56 +0200

    .

---
+++ ProjectUtils/Assets/ProjectUtils/Helpers/CoroutineControllerNonStatic.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectUtils.Helpers
+{
+    public class CoroutineControllerNonStatic : MonoBehaviour
+    {
+
+        public static CoroutineControllerNonStatic Instance;
+        Dictionary<string,IEnumerator> _routines = new Dictionary<string,IEnumerator>(100);
+
+        [RuntimeInitializeOnLoadMethod( RuntimeInitializeLoadType.BeforeSceneLoad )]
+        private void Awake()
+        {
+            if(Instance != null) Destroy(gameObject);
+            else
+            {
+                Instance = this;
+            }
+        }
+
+        public Coroutine Start ( IEnumerator routine ) => StartCoroutine( routine );
+        public Coroutine Start ( IEnumerator routine , string id )
+        {
+            var coroutine = StartCoroutine( routine );
+            if( !_routines.ContainsKey(id) ) _routines.Add( id , routine );
+            else
+            {
+                StopCoroutine( _routines[id] );
+                _routines[id] = routine;
+            }
+            return coroutine;
+        }
+        public void Stop ( IEnumerator routine ) => StopCoroutine( routine );
+        public void Stop ( string id )
+        {
+            if( _routines.TryGetValue(id,out var routine) )
+            {
+                StopCoroutine( routine );
+                _routines.Remove( id );
+            }
+            else Debug.LogWarning($"coroutine '{id}' not found");
+        }
+        public void StopAll () => StopAllCoroutines();
+    
+    }
+}
+
